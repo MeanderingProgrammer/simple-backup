7d4641e20e59fb8bf38bbbbd82e55956a2c77354
MeanderingProgrammer <suslikovvd@gmail.com>
2023-01-24 05:15:29 -0800

    Add an initial icon, needed to be careful with using RGBA format specifically, not png.

--- README.md
+++ README.md
@@ -37,3 +37,4 @@ C:\Users\vsusl\Documents\scripts\backup-test
 ## Some Useful Resources
 
 * https://codereview.stackexchange.com/questions/272387/rust-cli-tool-to-synchronize-files-to-s3
+* https://github.com/deadliner-app/Deadliner/tree/main
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,26 @@ use simple_backup::ui::app;
 use dioxus_desktop::{
     Config,
     WindowBuilder,
+    tao::window::Icon,
 };
 
 fn main() {
     let window = WindowBuilder::new()
         .with_title("Simple Backup Tool")
-        .with_window_icon(None);
+        .with_window_icon(Some(get_icon()));
 
     dioxus_desktop::launch_cfg(
         app::entry_point,
         Config::new().with_window(window),
     );
+}
+
+fn get_icon() -> Icon {
+    let icon_bytes = include_bytes!("../assets/icon.ico").to_vec();
+
+    let total_pixels = icon_bytes.len() / 4;
+    let side_length_float = (total_pixels as f64).sqrt();
+    let side_length = side_length_float as u32;
+
+    Icon::from_rgba(icon_bytes, side_length, side_length).unwrap()
 }
